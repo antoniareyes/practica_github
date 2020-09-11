17fae5ff6308d346a3ca75015aa42556248f9c7f
Simooc <sconejerosp@gmail.com>
2020-09-10 21:53:16 -0300

    Clase Pedidos

---
+++ practica_github/src/practica_github/Pedidos.java
@@ -0,0 +1,26 @@
+
+package practica_github;
+
+/**
+ *
+ * @author simoo
+ */
+public class Pedidos {
+    
+    private String producto;
+    private Integer cantidad;
+    private Long solicitud;
+
+    public Pedidos() {
+    }
+    
+    
+    public void recibirPedido(){
+        
+    }
+    
+    public void registroPedido(){
+        
+    }
+            
+}
