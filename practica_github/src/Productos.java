f1fb1fb223183fc0cfcef1658997b8bdf11c6cb5
eneidaguzman <eneidaguzman8@gmail.com>
2020-09-10 22:00:41 -0300

    Mensaje Eneida

---
+++ practica_github/src/Productos.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Eneida
+ */
+public class Productos {
+     private int id_producto;
+     private String nombre_producto;
+     private int precio_producto;
+     private int cantidad_producto;
+     private String nombre_proveedor;
+    
+}
